b6fc27c23e3d96dabecd0b2e22ddd6cb12245c32
JoseTobias <tobiassouza0@gmail.com>
2020-03-28 17:51:35 -0300

    doing template

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <login msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style>
+body{
+	margin:0;
+	color:#6a6f8c;
+	background:#c8c8c8;
+	font:600 16px/18px 'Open Sans',sans-serif;
+}
+
+*,:after,:before{box-sizing:border-box}
+.clearfix:after,.clearfix:before{content:'';display:table}
+.clearfix:after{clear:both;display:block}
+a{color:inherit;text-decoration:none}
+</style>
