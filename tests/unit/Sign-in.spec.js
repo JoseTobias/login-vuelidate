b8aa89e100667b177cfbb8dbb2f9a9ba2b4ec25f
JoseTobias <tobiassouza0@gmail.com>
2020-04-01 11:53:30 -0300

    more tests on Sign-in

--- tests/unit/Sign-in.spec.js
+++ tests/unit/Sign-in.spec.js
@@ -1,13 +1,53 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import { shallowMount } from '@vue/test-utils'
 import SignIn from '@/components/Sign-in.vue'
-import Vuelidate from 'vuelidate'
 
 Vue.use(Vuelidate)
 
 describe('Sign-in.vue', () => {
+  
+  let wrapper
+
+  beforeAll(() => {
+    wrapper = shallowMount(SignIn)
+  })
+
   it('Vue instance', () => {
-    const wrapper = shallowMount(SignIn)
     expect(wrapper.isVueInstance(wrapper)).toBeTruthy()
+  })
+
+  test('should $invalid == false when all fields are filled', () => {
+    setData(wrapper)
+    expect(wrapper.vm.$v.$invalid).toBeFalsy()
+  })
+
+  test('should $invalid == true in first', () => {
+    resetData(wrapper)
+    expect(wrapper.vm.$v.$invalid).toBeTruthy()
+  })
+
+  test('The event do-sign-in is fired when submit is executed', () => {
+    setData(wrapper)
+    wrapper.vm.submit()
+    const data = wrapper.emitted('do-sign-in')[0][0]
+    expect(data).toEqual({
+      username: 'test',
+      password: 'test',
+      keepSignedIn: false
+    })
+  })
+  
+
+  const setData = wrapper => wrapper.setData({
+    username: 'test',
+    password: 'test',
+    keepSignedIn: false
+  })
+
+  const resetData = wrapper => wrapper.setData({
+    username: '',
+    password: '',
+    keepSignedIn: false
   })
 })
