6401e04b5ded66f6e390610f10e7ace3d373f1f1
JoseTobias <tobiassouza0@gmail.com>
2020-04-06 11:09:30 -0300

    starting E2E tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,6 +3,11 @@
 describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+    cy.get('#sign-in-user').type('test')
+    cy.get('#sign-in-pass').type('test')
+    cy.get('label[for=check]').click()
+    cy.get('label[for=check]').click()
+    cy.get('input[value="Sign In"]').click()
+    cy.get('label[for=tab-2]').click()
   })
 })
